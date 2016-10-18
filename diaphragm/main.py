376be8d29db0919238578240c3aaf7b6919999f9
niemandkun <niemandkun@yandex.ru>
2016-10-18 21:52:13 +0500

    load config and render template in index()

--- diaphragm/main.py
+++ diaphragm/main.py
@@ -2,12 +2,16 @@
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__, static_folder="static")
+app.config.from_object('config.DebugConfig')
+
 
 @app.route("/")
 @app.route("/index.html")
 def index():
-    return "Hello from Flask!"
+    return render_template("layout.html")
+
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 8080)
