8c11b3ce5ca353c1ac7426535ba8ddd3f941994a
niemandkun <niemandkun@yandex.ru>
2017-02-19 14:22:45 +0500

    - board and gallery

--- diaphragm/factory.py
+++ diaphragm/factory.py
@@ -5,8 +5,8 @@ def create_app(config):
     app = Flask(__name__, static_folder='static')
     app.config.from_object(config)
 
-    from diaphragm.board.utils import boardify
-    app.jinja_env.filters['boardify'] = boardify
+    # from diaphragm.board.utils import boardify
+    # app.jinja_env.filters['boardify'] = boardify
 
     from diaphragm.about.views import about
     app.register_blueprint(about)
@@ -14,11 +14,11 @@ def create_app(config):
     from diaphragm.root.views import root
     app.register_blueprint(root)
 
-    from diaphragm.gallery.views import gallery
-    app.register_blueprint(gallery)
+    # from diaphragm.gallery.views import gallery
+    # app.register_blueprint(gallery)
 
-    from diaphragm.board.views import board
-    app.register_blueprint(board)
+    # from diaphragm.board.views import board
+    # app.register_blueprint(board)
 
     from diaphragm.counter.views import counter
     app.register_blueprint(counter)
